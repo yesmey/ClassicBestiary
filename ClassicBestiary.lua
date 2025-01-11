d6a33d315177e26a56a292949194bde2832085d5
Jesper Meyer <yesmey@users.noreply.github.com>
2025-01-11 01:42:22 +0100

    Database rewrite. Only show neutral or hostile units. Add attack speed information

--- ClassicBestiary.lua
+++ ClassicBestiary.lua
@@ -6,8 +6,28 @@ local Spell = Spell
 local function onTooltip()
   local name, id = GameTooltip:GetUnit()
   local unitType, _, _, _, _, npcIDStr = strsplit("-", UnitGUID(id))
+
+  -- Return if its not a Creature
   if not npcIDStr or unitType ~= "Creature" then
     return
+  end
+  
+  -- Return if its not neutral or hostile
+  local reaction = UnitReaction("player", id)
+  if reaction and reaction >= 5 then
+    return
+  end
+
+  if IsControlKeyDown() then
+    mainSpeed, offSpeed = UnitAttackSpeed(id);
+    if mainSpeed < 2 or offSpeed ~= nil then
+      GameTooltip:AddLine("Fast Attack", 1, 0.3, 0.3);
+    elseif mainSpeed > 2 then
+      GameTooltip:AddLine("Slow Attack", 0.2, 1, 0.5);
+    end
+    else then
+      GameTooltip:AddLine("Normal Attack", 0.2, 1, 0.5);
+    end
   end
 
   local abilities = DB.map[tonumber(npcIDStr)]
@@ -16,58 +36,48 @@ local function onTooltip()
   end
 
   for _, spellID in ipairs(abilities) do
-    local spellName, _rank, _icon, _castTime, minRange, maxRange = GetSpellInfo(spellID);
+    local spellName, _, _, _, _, _ = GetSpellInfo(spellID);
     if spellName then
       local icon = "";
       local texture = GetSpellTexture(spellID);
       if (texture ~= nil) then
         icon = "|T" .. texture .. ":0|t";
       end
-      
-      local range = ""
-
-      if maxRange and maxRange > 0 and maxRange < 50 then
-        if (minRange and minRange > 0) then
-          range = "(" .. tostring(minRange) .. " - " .. tostring(maxRange) .. " yd)"
-        else
-          range = "(" .. tostring(maxRange) .. " yd)"
-        end
-      end
-  
-      GameTooltip:AddLine(icon .. " " .. spellName .. " " .. range)
+	  
       if IsControlKeyDown() then
-        local spell = Spell:CreateFromSpellID(spellID)
+        local spell = Spell:CreateFromSpellID(spellID);
         spell:ContinueOnSpellLoad(function()
-          GameTooltip:AddLine(spell:GetSpellDescription(), 0.8, 0.8, 0.8, true)
+		  GameTooltip:AddLine(icon .. " " .. spellName)
+          GameTooltip:AddLine(spell:GetSpellDescription(), 0.7, 0.7, 0.7, true);
           if GameTooltip:GetWidth() > 700 then
-            GameTooltip:SetWidth(700)
+            GameTooltip:SetWidth(700);
           end
-          GameTooltip:Show() -- Refresh size
-        end)
+          GameTooltip:Show();
+        end);
       end
     end
   end
   
   if not IsControlKeyDown() then
-    GameTooltip:AddLine("(Ctrl for details)", 0.8, 0.8, 0.8)
+    GameTooltip:AddLine("(Ctrl for details)", 0.8, 0.8, 0.8);
   end
   
   if GameTooltip:GetWidth() > 700 then
-    GameTooltip:SetWidth(700)
+    GameTooltip:SetWidth(700);
   end
-  end
-  
-  local frame = CreateFrame("Frame", "ClassicBestiaryEvents")
-  frame:SetScript("OnEvent", function(self, event, ...)
-    if event == "PLAYER_ENTERING_WORLD" then
-      GameTooltip:HookScript("OnTooltipSetUnit", onTooltip)
-      self:UnregisterEvent("PLAYER_ENTERING_WORLD")
-    elseif event == "MODIFIER_STATE_CHANGED" then
-      if UnitExists("mouseover") then
-        GameTooltip:SetUnit("mouseover");
-      end
-    end
-  end)
+end
 
-frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-frame:RegisterEvent("MODIFIER_STATE_CHANGED")
+local frame = CreateFrame("Frame", "ClassicBestiaryEvents");
+frame:SetScript("OnEvent", function(self, event, ...)
+  if event == "PLAYER_ENTERING_WORLD" then
+    GameTooltip:HookScript("OnTooltipSetUnit", onTooltip);
+    self:UnregisterEvent("PLAYER_ENTERING_WORLD");
+  elseif event == "MODIFIER_STATE_CHANGED" then
+    if UnitExists("mouseover") then
+  	  GameTooltip:SetUnit("mouseover");
+    end
+  end
+end);
+
+frame:RegisterEvent("PLAYER_ENTERING_WORLD");
+frame:RegisterEvent("MODIFIER_STATE_CHANGED");
--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Displays creature ability information in the game tooltip when available. Hold Control while hovering over a unit to display descriptions for spells.
 
-![Collapsed view](assets/bestiary-collapse.png)
-![Expanded view](assets/bestiary-expand.png)
+This fork came about because I though the original had too many issues. And since the db.lua generation is closed source I had to create one myself. Mine uses a variety of different data sources.
 
 Repo: https://github.com/yesmey/ClassicBestiary
-
